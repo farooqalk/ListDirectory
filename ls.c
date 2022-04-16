822c479068101a72904ddd072cefb8936ca1155b
= <=>
2022-04-16 05:33:07 -0400

    first commit

---
+++ ls.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+    // Run dir + supplied arg(S)
+    if (argc == 2)
+    {
+        // printf("The argument supplied is %s\n", argv[1]);
+
+        // First part of the command is always the same
+        char exp[] = "dir ";
+        // Create a new char array that has the size of the command, and length of the supplied argument
+        char target[sizeof exp + sizeof argv[1]];
+        // Copies contents of foo to target buffer
+        strcpy(target, exp);
+        // Appends contents of bar to target buffer
+        strcat(target, argv[1]);
+        // Run the command
+        system(target);
+    }
+    else if (argc > 2)
+    {
+        printf("Too many arguments supplied.\n");
+    }
+    // If no arguments are supplied, open the current directory in file explorer
+    else
+    {
+        // First part of the command is always the same
+        char cmnd[] = "dir ";
+        // Create a new char array that has the size of the command, and supplied directory
+        char target[sizeof cmnd];
+        // Copies contents of foo to target buffer
+        strcpy(target, cmnd);
+        // Appends contents of bar to target buffer
+        strcat(target, "");
+        // Run the command
+        system(target);
+    }
+    return 0;
+}
